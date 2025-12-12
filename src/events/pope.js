0a2412f618e5a5b4129937f0b0139f8ed09cf3d7
LunarWanderer月 <76808845+lunarwanderer13@users.noreply.github.com>
2025-12-12 09:02:57 +0100

    Removed allowing binary to be typed with spaces
    Spaces lengthen the string which in return makes .slice() return the incorrect string

--- src/events/pope.js
+++ src/events/pope.js
@@ -20,7 +20,7 @@ export function execute(message) {
     const pope_list = JSON.parse(fs.readFileSync("src/logs/pope.json"))
 
     if (message.content.slice(0, 4) === "2137"
-        || message.content.slice(0, 14).replace(/ /g, "") === "0b100001011001"
+        || message.content.slice(0, 14) === "0b100001011001"
         || message.content.slice(0, 6) === "0o4131"
         || message.content.slice(0, 5) === "0x859"
         || message.content.slice(0, 21).toLowerCase() === "dwa jeden trzy siedem") {
