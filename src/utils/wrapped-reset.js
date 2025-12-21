cbc5987bf904eee6484d6503e8455474bddd8796
lunarwanderer13 <lunarwanderer13@gmail.com>
2025-12-21 18:19:56 +0100

    Remove unnecessary file reading

--- src/utils/wrapped-reset.js
+++ src/utils/wrapped-reset.js
@@ -7,7 +7,6 @@ function log(level, message) {
 }
 
 schedule.scheduleJob("0 0 1 1 *", () => {
-    const data = JSON.parse(fs.readFileSync("src/logs/wrapped.json"))
     fs.writeFileSync("src/logs/wrapped.json", "")
     log("LOG", `Resetted the wrapped.json file.`)
 })
