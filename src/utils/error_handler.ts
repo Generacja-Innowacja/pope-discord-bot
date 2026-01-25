49f23446aa619808c4e18afbb7761ea09420dd63
lunarwanderer13 <lunarwanderer13@gmail.com>
2026-01-25 13:18:42 +0100

    Update channel case

--- src/utils/error_handler.ts
+++ src/utils/error_handler.ts
@@ -14,25 +14,40 @@ async function sendReport(interaction: ChatInputCommandInteraction, embed: Embed
     owner.send({ embeds: [embed] })
 }
 
-export function error(interaction: ChatInputCommandInteraction, type: string = "error", report: boolean = false): void {
+export async function error(interaction: ChatInputCommandInteraction, type: string = "error", report: boolean = false): Promise<void> {
     const errorEmbed: EmbedBuilder = new EmbedBuilder()
         .setColor(Color.accent)
         .setTitle("Error code 418: I'm a teapot")
 
+    let description: string = ""
+
     switch (type.toLowerCase()) {
         case "slashcommand":
-            errorEmbed.setDescription("Command not found.")
+            description = "Command not found."
             break
         case "channel":
-            errorEmbed.setDescription(`This command can only be ran in the ${interaction.client.channels.fetch(process.env.CHANNEL_ID!)} channel.`)
+            const guild = await interaction.client.guilds.fetch(process.env.GUILD_ID!)
+            if (!guild || interaction.guild !== guild) {
+                description = `This command can only be ran in the ${guild.name} server.`
+                break
+            }
+
+            const channel = await guild.channels.fetch(process.env.CHANNEL_ID!)
+            if (channel) {
+                description = `This command can only be ran in the ${channel.url} channel.`
+            } else {
+                description = "This command can't be ran in this channel."
+            }
             break
         case "emoji":
-            errorEmbed.setDescription("Unable to access the application's emoji.")
+            description = "Unable to access the application's emoji."
             break
 
         default:
-            errorEmbed.setDescription("Unexpected error has occured.")
+            description = "Unexpected error has occured."
     }
+
+    errorEmbed.setDescription(description)
 
     interaction.reply({ embeds: [errorEmbed], flags: MessageFlags.Ephemeral })
     if (report) sendReport(interaction, errorEmbed)
