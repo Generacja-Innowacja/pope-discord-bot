810ebc6ae94d8370c24cc6cbfba4da1447133072
lunarwanderer13 <lunarwanderer13@gmail.com>
2026-02-06 16:01:25 +0100

    Refactor if statement

--- src/events/clientReady.ts
+++ src/events/clientReady.ts
@@ -4,9 +4,7 @@ import "dotenv/config"
 
 export default (client: Client): void => {
     client.on("clientReady", async () => {
-        if (!client.user || !client.application) {
-            return
-        }
+        if (!client.user || !client.application) return
 
         const rest = new REST().setToken(process.env.TOKEN!)
         await rest.put(Routes.applicationCommands(client.user.id), { body: Commands.map(command => command.data.toJSON()) })
