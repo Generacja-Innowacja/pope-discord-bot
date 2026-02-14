1eb3a2b7cefb9bb4af3fe3f6b1f4753d7dc8fc6b
lunarwanderer13 <lunarwanderer13@gmail.com>
2026-02-14 14:44:20 +0100

    Remove commands on startup to avoid double commands

--- src/events/clientReady.ts
+++ src/events/clientReady.ts
@@ -5,6 +5,7 @@ export default (client: Client): void => {
     client.on("clientReady", async () => {
         if (!client.user || !client.application) return
 
+        client.application.commands.set([])
         const rest = new REST().setToken(process.env.TOKEN!)
         await rest.put(Routes.applicationCommands(client.user.id), { body: Commands.map(command => command.data.toJSON()) })
 
